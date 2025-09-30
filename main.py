99b768e279c7e147a8891c112e1ced2e448cce4d
Abdul-abiola <abdulrahamanabdulrahaman03@gmail.com>
2025-09-30 16:06:57 -0700

    Add files via upload
    uploaded version 1

---
+++ main.py
@@ -0,0 +1,30 @@
+# result = "good morning"
+# print(result)
+followers = ["david","mic","mary","john","sam","david"]
+print(list(enumerate(followers)))
+# print(tuple(enumerate(followers)))
+# print(dir(followers))
+# # new_follower = "shegun"
+# # followers.append(new_follower)
+# # print(followers)
+# # new_follower = "daniel"
+# # followers.append(new_follower)
+# # print(followers)
+
+# # names = ["david","mic","mary","john","sam"]
+# # new_name = input("enter your name: ")
+# # names.append(new_name)
+# # print(names)
+# # followers.clear()
+# # print(followers)
+# # del(followers)
+# # print(followers)
+# # count = followers.count("david")
+# # print(count)
+# new_follower = "shegun", "vic",  "joy"
+# followers.append(new_follower)
+# print(followers)
+
+# new_follower = "daniel"
+# followers.append(new_follower)
+# print(followers)
